e6dcd45b1d806073c201829d481246e0140d53d6
Adrian <flojd_xl@wp.pl>
2020-06-21 16:39:18 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battle_of_cards_aceshigh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cthulhu Fhtang!");
+        }
+    }
+}
