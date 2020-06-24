ddc2674be63fee095b7fc63e5087d69bb316cea3
endrde <38038675+eNdrD@users.noreply.github.com>
2020-06-25 00:56:30 +0200

    Initialized abstract class Player and IPlayer interface

---
+++ Source/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCardsAcesHigh
+{
+    abstract class Player : IEquatable<Player>
+    {
+        private string playerName;
+        private Deck playerHand;
+
+
+        public Player(string name)
+        {
+
+        }
+
+        public bool Equals(Player other)
+        {
+            if (other == null)
+                return false;
+
+            if (this.playerName == other.playerName)
+                return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Player playerObj = obj as Player;
+            if (playerObj == null)
+                return false;
+            else
+                return Equals(playerObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(playerName, playerHand);
+        }
+    }
+}
