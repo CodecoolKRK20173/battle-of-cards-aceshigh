6a0672897bda3f5d209effc3d2c4e9a6ce2ac40e
Adrian Pacholarz <flojd_xl@wp.pl>
2020-06-30 00:31:52 +0200

    Implement methods in "Table" class to check the winner of the round

--- Source/Table.cs
+++ Source/Table.cs
@@ -10,14 +10,66 @@ namespace BattleOfCardsAcesHigh.Source
     class Table
     {
         private List<Card> _playedCards;
-        private List<Player> _allPlayers;
+        private List<Card> _winnersCards;
+        private LinkedList<Player> _allPlayers;
+        private Dealer _januszDealer;
 
-        public Table()
+        public Table(LinkedList<Player> allPlayers)
         {
-            Dealer janusz = new Dealer(_allPlayers.Count);
-            janusz.DealCards(_allPlayers);
-            _playedCards.Sort(janusz);
+            this._allPlayers = allPlayers;
+            this._januszDealer = new Dealer(_allPlayers.Count);
+            _januszDealer.DealCards(_allPlayers);
              
+        }
+
+
+        public void SortPlayedCards(List<Card> playedCards, string statToCompare)
+        {
+            foreach (Card card in playedCards)
+            {
+                card.SetStatToCompare(statToCompare);
+            }
+
+            playedCards.Sort(_januszDealer);
+
+            this._playedCards = playedCards;
+
+        }
+
+        public bool IsONeWinner()
+        {
+            if (_playedCards[0].CompareTo(_playedCards[1]) == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Player GetWinner()
+        {
+            return _playedCards[0].GetPlayedBy();
+        }
+
+        public List<Player> GetAllWinners()
+        {
+            var allWinners = new List<Player>();
+
+            foreach (Card card in _playedCards)
+            {
+                if (card.CompareTo(_playedCards[0]) == 0)
+                {
+                    allWinners.Add(card.GetPlayedBy());
+                }
+            }
+            return allWinners;
+        }
+
+
+
+        public void PassCardsToWinner(Player winner)
+        {
+            winner.AddCardsToHand(_winnersCards);
         }
     }
 }
