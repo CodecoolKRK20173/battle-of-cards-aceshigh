bea8534cc1c1b55639adb8be587120f9e3ccb82e
kettu0 <aneta.kruc@gmail.com>
2020-07-01 01:10:53 +0200

    create Square class

---
+++ Source/View.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BattleOfCardsAcesHigh
+{
+    public class View
+    {
+        public static void PrintTitle()
+        {
+            using (StreamReader stream = File.OpenText(@"\Files\title.txt"))
+            {
+                String gameTitle = "";
+
+                while ((gameTitle = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(gameTitle);
+                }
+            }
+        }
+
+        public static void PrintCardFaceDown()
+        {
+            int cardWidth = 19;
+            int cardHeight = 12;
+
+        }
+
+        public static void PrintCommentField()
+        {
+
+        }
+    }
+}
